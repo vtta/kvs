2f9c5d8c43887232f760230d28576304d842d92b
vtta <vtta0124@gmail.com>
2020-01-25 17:04:59 +0800

    Project 1: make tests compile
    Signed-off-by: vtta <vtta0124@gmail.com>

---
+++ src/lib.rs
@@ -0,0 +1,22 @@
+pub struct KvStore {}
+
+impl KvStore {
+    pub fn new() -> Self {
+        todo!()
+    }
+
+    /// Set the value of a string key to a fstring
+    pub fn set(&mut self, key: String, value: String) {
+        todo!()
+    }
+
+    /// Get the string value of the a string key. If the key does not exist,
+    pub fn get(&mut self, key: String) -> Option<String> {
+        todo!()
+    }
+
+    /// Remove a given key.
+    pub fn remove(&mut self, key: String) {
+        todo!()
+    }
+}
